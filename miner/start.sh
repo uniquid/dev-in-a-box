5080a8f7bd81f7f598a60917d89438f3cdcf2c38
aleclofabbro <agiansanti@uniquid.com>
2017-10-30 11:06:21 +0100

    lasciamo questo branch appeso, per poter rilasciare una versione che utilizzi la blockchain di uniquid

--- miner/start.sh
+++ miner/start.sh
@@ -1,6 +1,6 @@
 # echo "rpcbind=$(getent hosts miner | awk '{ print $1 }')
 # rpcallowip=$(getent hosts tabacchi | awk '{ print $1 }')" | cat /bitcoin/.bitcoin/bitcoin.conf.part - > /bitcoin/.bitcoin/bitcoin.conf
-echo "rpcbind=172.18.0.10
-rpcallowip=172.18.0.0/16" | cat /bitcoin/.bitcoin/bitcoin.conf.part - > /bitcoin/.bitcoin/bitcoin.conf
-cat /bitcoin/.bitcoin/bitcoin.conf
-bitcoind
+# echo "rpcbind=172.18.0.10
+# rpcallowip=172.18.0.0/16" | cat /bitcoin/.bitcoin/bitcoin.conf.part - > /bitcoin/.bitcoin/bitcoin.conf
+# cat /bitcoin/.bitcoin/bitcoin.conf
+bitcoind -conf=/bitcoin/bitcoin.conf
--- quickstart/start-tank-java.sh
+++ quickstart/start-tank-java.sh
@@ -1,2 +1,2 @@
 docker build -t uniquid/dev-tank-java ../tank-java
-docker run -d --network uniquid_default uniquid/dev-tank-java
+docker run -d --network uniquid_default --name=java_tank_$1 -v=java_tank_$1_volume:/db/ uniquid/dev-tank-java 
